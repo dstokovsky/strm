080e90b5b28f3c807f7c709b35caaa806161315a
Denis Stokovsky <denis@gravity4.com>
2015-08-22 17:41:08 +0300

    Added retrieving all user's friends.

--- index.php
+++ index.php
@@ -174,23 +174,23 @@ $app->delete('/api/users/{id:[0-9]+}', function ($id) use ($app) {
     return $response;
 });
 
-$app->post('/api/messages/{id:[0-9]+}/{id:[0-9]+}', function ($author_id, $recipient_id) use ($app) {
+$app->post('/api/messages/{author_id:[0-9]+}/{recipient_id:[0-9]+}', function ($author_id, $recipient_id) use ($app) {
     $message = $app->request->getJsonRawBody();
     $response = new Response();
     $content = ['code' => 404, 'status' => 'Not Found', 'data' => []];
-    if(!isset($message->text) || empty($message->text) || strlen($message->text))
+    if(!isset($message->text) || empty($message->text))
     {
         $content = ['code' => 500, 'status' => 'Internal Error', 'data' => 
             ['Invalid message text, it is not set or empty or longer than expected.']];
     }
     
-    $author = User::findFirst(['id' => $author_id]);
+    $author = User::findFirst(['conditions' => 'id = ?1', 'bind' => [1 => $author_id]]);
     if(empty($author))
     {
         $content['data'][] = 'There is no user with such author id.';
     }
     
-    $recipient = User::findFirst(['id' => $recipient_id]);
+    $recipient = User::findFirst(['conditions' => 'id = ?1', 'bind' => [1 => $recipient_id]]);
     if(empty($recipient))
     {
         $content['data'][] = 'There is no user with such recipient id.';
@@ -234,16 +234,16 @@ $app->post('/api/messages/{id:[0-9]+}/{id:[0-9]+}', function ($author_id, $recip
     return $response;
 });
 
-$app->post('/api/friends/{id:[0-9]+}/{id:[0-9]+}', function ($user_id, $friend_id) use ($app) {
+$app->post('/api/friends/{user_id:[0-9]+}/{friend_id:[0-9]+}', function ($user_id, $friend_id) use ($app) {
     $response = new Response();
     $content = ['code' => 404, 'status' => 'Not Found', 'data' => []];
-    $user = User::findFirst(['id' => $user_id]);
+    $user = User::findFirst(['conditions' => 'id = ?1', 'bind' => [1 => $user_id]]);
     if(empty($user))
     {
         $content['data'][] = 'There is no user with such user id.';
     }
     
-    $friend = User::findFirst(['id' => $friend_id]);
+    $friend = User::findFirst(['conditions' => 'id = ?1', 'bind' => [1 => $friend_id]]);
     if(empty($friend))
     {
         $content['data'][] = 'There is no user with such friend id.';
@@ -251,8 +251,33 @@ $app->post('/api/friends/{id:[0-9]+}/{id:[0-9]+}', function ($user_id, $friend_i
     
     if(empty($content['data']))
     {
-        $friends = UsersFriends::findFirst(['user_id' => $friend_id, 'friend_id' => $user_id]);
+        $friends = UsersFriends::findFirst(['conditions' => 'user_id = ?1 AND friend_id=?2', 
+            'bind' => [1 => $friend_id, 2 => $user_id]]);
         $status_value = !empty($friends) ? UsersFriends::FRIEND_STATUS : UsersFriends::SUBSCRIBER_STATUS;
+        if($status_value === UsersFriends::FRIEND_STATUS)
+        {
+            $phql = "UPDATE UsersFriends SET status=:status: WHERE user_id = :user_id: AND friend_id = :friend_id:";
+            $status = $app->modelsManager->executeQuery($phql, [
+                'status' => $status_value,
+                'user_id' => $friend_id,
+                'friend_id' => $user_id,
+            ]);
+            if (!$status->success())
+            {
+                $errors = array();
+                foreach ($status->getMessages() as $message)
+                {
+                    $errors[] = $message->getMessage();
+                }
+                $content = [
+                    'code' => 409,
+                    'status' => 'Conflict',
+                    'data' => $errors,
+                ];
+                $response->setJsonContent($content);
+                return $response;
+            }
+        }
         $phql = 'INSERT INTO UsersFriends (user_id, friend_id, status, created_at, updated_at) ' . 
             'VALUES (:user_id:, :friend_id:, :status:, :created_at:, :updated_at:)';
         $status = $app->modelsManager->executeQuery($phql, array(
@@ -281,6 +306,35 @@ $app->post('/api/friends/{id:[0-9]+}/{id:[0-9]+}', function ($user_id, $friend_i
                 'code' => 409,
                 'status' => 'Conflict',
                 'data' => $errors,
+            ];
+        }
+    }
+    
+    $response->setJsonContent($content);
+    return $response;
+});
+
+$app->get('/api/friends/{id:[0-9]+}', function ($id) use ($app) {
+    $response = new Response();
+    $content = ['code' => 404, 'status' => 'Not Found', 'data' => []];
+    $user = User::findFirst(['conditions' => 'id = ?1', 'bind' => [1 => $id]]);
+    if(empty($user))
+    {
+        $content['data'][] = 'There is no such user.';
+    }
+    
+    if(empty($content['data']))
+    {
+        $phql = "SELECT User.* FROM User INNER JOIN UsersFriends ON User.id=UsersFriends.friend_id WHERE UsersFriends.user_id = :id:";
+        $friends = $app->modelsManager->executeQuery($phql, ['id' => $id]);
+        $content = ['code' => 200, 'status' => 'Ok', 'data' => []];
+        foreach ($friends as $friend)
+        {
+            $content['data'][] = [
+                'id' => $friend->id,
+                'email' => $friend->email,
+                'first_name' => $friend->first_name,
+                'second_name' => $friend->second_name,
             ];
         }
     }
